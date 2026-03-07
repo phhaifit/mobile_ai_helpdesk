61d510116da5f9941d1145cacbbb4f8d8cfb672d
nban22 <nguyenbaan662004@gmail.com>
2026-03-07 19:15:55 +0700

    feat(bootstrap): set up mock data layer and repository pattern
    - Add base UseCase<T, P> abstract class in core/domain
    - Add Ticket entity with JsonSerializable and status/priority enums
    - Add TicketRepository interface in domain layer (pure Dart)
    - Add GetTicketsUseCase extending UseCase base class
    - Add MockTicketRepositoryImpl with 5 sample tickets and simulated delays
    - Register TicketRepository and GetTicketsUseCase in DI modules
    - Run build_runner to generate ticket.g.dart
    
    Refs #16

--- lib/data/di/module/repository_module.dart
+++ lib/data/di/module/repository_module.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 
 import 'package:ai_helpdesk/data/repository/setting/setting_repository_impl.dart';
+import 'package:ai_helpdesk/data/repository/ticket/mock_ticket_repository_impl.dart';
 import 'package:ai_helpdesk/data/sharedpref/shared_preference_helper.dart';
 import 'package:ai_helpdesk/domain/repository/setting/setting_repository.dart';
+import 'package:ai_helpdesk/domain/repository/ticket/ticket_repository.dart';
 
 import '../../../di/service_locator.dart';
 
@@ -11,6 +13,10 @@ class RepositoryModule {
     // repository:--------------------------------------------------------------
     getIt.registerSingleton<SettingRepository>(
       SettingRepositoryImpl(getIt<SharedPreferenceHelper>()),
+    );
+
+    getIt.registerSingleton<TicketRepository>(
+      MockTicketRepositoryImpl(),
     );
   }
 }
--- lib/domain/di/module/usecase_module.dart
+++ lib/domain/di/module/usecase_module.dart
@@ -1,12 +1,15 @@
 import 'dart:async';
 
+import 'package:ai_helpdesk/domain/repository/ticket/ticket_repository.dart';
+import 'package:ai_helpdesk/domain/usecase/ticket/get_tickets_usecase.dart';
+
+import '../../../di/service_locator.dart';
+
 class UseCaseModule {
   static Future<void> configureUseCaseModuleInjection() async {
     // use cases:---------------------------------------------------------------
-    // Register use cases here as features are added.
-    // Example:
-    // getIt.registerSingleton<GetFeatureUseCase>(
-    //   GetFeatureUseCase(getIt<FeatureRepository>()),
-    // );
+    getIt.registerSingleton<GetTicketsUseCase>(
+      GetTicketsUseCase(getIt<TicketRepository>()),
+    );
   }
 }
