54f7cef5e6f942207c760af2f8411ed854a76111
Khoi Pham <dinhkhoi26031997@gmail.com>
2026-03-23 23:43:59 +0700

    feat: adding mechanism tracking for all screen

--- lib/presentation/my_app.dart
+++ lib/presentation/my_app.dart
@@ -43,7 +43,6 @@ class MyApp extends StatelessWidget {
             GlobalWidgetsLocalizations.delegate,
             GlobalCupertinoLocalizations.delegate,
           ],
-          home: const MainScreen(),
         );
       },
     );
